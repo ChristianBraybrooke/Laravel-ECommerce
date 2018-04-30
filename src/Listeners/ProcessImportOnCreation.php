45d8aefdf2ec63bd91de8c5d844557606d46a12a
Christian Braybrooke <chris@purplemountmedia.com>
2018-04-30 12:35:57 +0100

    Remove imports queue

--- src/Listeners/ProcessImportOnCreation.php
+++ src/Listeners/ProcessImportOnCreation.php
@@ -32,7 +32,7 @@ class ProcessImportOnCreation implements ShouldQueue
     public function handle($event)
     {
         if (isset($event->model->status) && $event->model->status === 'Pending') {
-            ProcessImport::dispatch($event->model)->onQueue('imports');
+            ProcessImport::dispatch($event->model);
         }
 
         // $admin_ids = Setting::get('Admin Notifications');
