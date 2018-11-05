2d15b4db655c9d3d8b800d2771c73c706344e6cc
Christian Braybrooke <chris@purplemountmedia.com>
2018-11-05 15:41:41 +0000

    Frontend changes

--- src/ECommerceServiceProvider.php
+++ src/ECommerceServiceProvider.php
@@ -39,7 +39,7 @@ class ECommerceServiceProvider extends LaravelServiceProvider
      */
     protected $defer = false;
 
-    const VERSION = '0.0.65';
+    const VERSION = '0.0.67';
 
     /**
      * Bootstrap the application events.
