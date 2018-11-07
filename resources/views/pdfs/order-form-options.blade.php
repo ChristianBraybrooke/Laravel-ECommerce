e305e0ae64a47baf0f24f1795e6a3d200e9f246c
Christian Braybrooke <chris@purplemountmedia.com>
2018-11-07 08:49:24 +0000

    Count options

--- resources/views/pdfs/order-form-options.blade.php
+++ resources/views/pdfs/order-form-options.blade.php
@@ -1,12 +1,14 @@
 <ul class="options_list">
   @foreach ($form['sections']['data'] ?? [] as $key => $section)
-    <li class="cart_option"><span class="font-weight-bold">
+    @if (count($section['fields']['data']) >= 1)
+      <li class="cart_option"><span class="font-weight-bold">
         @foreach ($section['fields']['data'] as $key => $field)
-            @php
-              $value = $options[$field['name']]['value'] ?? null;
-            @endphp
-            @if ($value)<strong>{{ $field['name'] ?? '' }}</strong>: {{ $field['prepend'] ?? '' }}{{ $value }}{{ $field['append'] ?? '' }}@if (!$loop->last),@endif @endif
+          @php
+            $value = $options[$field['name']]['value'] ?? null;
+          @endphp
+          @if ($value)<strong>{{ $field['name'] ?? '' }}</strong>: {{ $field['prepend'] ?? '' }}{{ $value }}{{ $field['append'] ?? '' }}@if (!$loop->last),@endif @endif
         @endforeach
-    </li>
+      </li>
+    @endif
   @endforeach
 </ul>
--- src/ECommerceServiceProvider.php
+++ src/ECommerceServiceProvider.php
@@ -39,7 +39,7 @@ class ECommerceServiceProvider extends LaravelServiceProvider
      */
     protected $defer = false;
 
-    const VERSION = '0.0.71';
+    const VERSION = '0.0.72';
 
     /**
      * Bootstrap the application events.
