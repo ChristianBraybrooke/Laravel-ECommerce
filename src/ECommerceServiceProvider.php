9b747adb7d6dfb7f572f4f0e723b0b389f9fe4b2
Jake Causier <jake@purplemountmedia.com>
2019-05-14 09:38:38 +0100

    Fixed column widths on dynamic populated fields

--- src/ECommerceServiceProvider.php
+++ src/ECommerceServiceProvider.php
@@ -42,7 +42,7 @@ class ECommerceServiceProvider extends LaravelServiceProvider
      */
     protected $defer = false;
 
-    const VERSION = '0.0.99';
+    const VERSION = '0.1.00';
 
     /**
      * Bootstrap the application events.
