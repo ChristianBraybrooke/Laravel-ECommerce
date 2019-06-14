ace2fce1e87123b4e067148d468272fd864b41c6
Christian Braybrooke <chris@purplemountmedia.com>
2019-06-14 10:21:36 +0100

    Update framework support

--- src/ECommerceServiceProvider.php
+++ src/ECommerceServiceProvider.php
@@ -42,7 +42,7 @@ class ECommerceServiceProvider extends LaravelServiceProvider
      */
     protected $defer = false;
 
-    const VERSION = '0.1.04';
+    const VERSION = '0.1.05';
 
     /**
      * Bootstrap the application events.
