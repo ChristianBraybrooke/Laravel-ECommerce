e674c2b4ece38a484a44b468baa41fc9543a923b
Jake Causier <jake@purplemountmedia.com>
2019-06-21 15:24:17 +0100

    Potential fix for blue backgrounds on dialogs

--- resources/assets/admin-spa/admin.js
+++ resources/assets/admin-spa/admin.js
@@ -8,7 +8,7 @@ import locale from 'element-ui/lib/locale/lang/en'
 import VueClipboard from 'vue-clipboard2'
 import { codeConverter } from 'utils/currency'
 
-window.ecommerceConfig.web_version = '0.1.01'
+window.ecommerceConfig.web_version = '0.1.02'
 
 window.has = require('lodash.has')
 var numeral = require('numeral')
