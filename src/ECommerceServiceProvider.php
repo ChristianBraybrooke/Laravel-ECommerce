80455ce621994be6a00c3ccf304535895d046b7b
Christian Braybrooke <chris@purplemountmedia.com>
2019-08-05 17:26:38 +0100

    Npm update and changes

--- src/ECommerceServiceProvider.php
+++ src/ECommerceServiceProvider.php
@@ -42,7 +42,7 @@ class ECommerceServiceProvider extends LaravelServiceProvider
      */
     protected $defer = false;
 
-    const VERSION = '0.1.22';
+    const VERSION = '0.1.23';
 
     /**
      * Bootstrap the application events.
