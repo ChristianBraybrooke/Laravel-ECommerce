a3c5835f18e7dc3de587966fce3d748befff6f42
Jake Causier <jake@purplemountmedia.com>
2019-08-06 10:35:18 +0100

    Updated version number with previous merge conflicts

--- src/ECommerceServiceProvider.php
+++ src/ECommerceServiceProvider.php
@@ -42,7 +42,7 @@ class ECommerceServiceProvider extends LaravelServiceProvider
      */
     protected $defer = false;
 
-    const VERSION = '0.1.23';
+    const VERSION = '0.1.24';
 
     /**
      * Bootstrap the application events.
